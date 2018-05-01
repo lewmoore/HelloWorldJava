62d4f22d2120cf9ebe3ac503f1fa6f24ed496834
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-01 13:46:49 +0100

    Initialise git repo

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+                System.out.println(greeting.sayHello());
+    }
+}
